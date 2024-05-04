6c9714f9d9b91ec4747e9e52ba1b1a2840b71fd5
Ratan-Y-Mallya <161617838+Ratan-Y-Mallya@users.noreply.github.com>
2024-05-04 10:21:58 +0530

    Update ratanmallya.py

--- ASSIGNMENT 2/ratanmallya.py
+++ ASSIGNMENT 2/ratanmallya.py
@@ -8,7 +8,7 @@ hand_detector = mp.solutions.hands.Hands()
 drawing_utils = mp.solutions.drawing_utils
 screen_width, screen_height = pyautogui.size()
 index_y = 0
-while True:
+while not keyboard.is_pressed('r');
     _, frame = cap.read()
     frame = cv2.flip(frame, 1)
     frame_height, frame_width, _ = frame.shape
